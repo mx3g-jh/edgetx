323ad0c80794b50ee76b0ed65a940a3655d55b67
Peter Feerick <peter.feerick@gmail.com>
2021-08-02 14:15:14 +1000

    Make img buf only size of img not whole widget

--- radio/src/gui/colorlcd/widgets/modelbmp.cpp
+++ radio/src/gui/colorlcd/widgets/modelbmp.cpp
@@ -46,7 +46,7 @@ class ModelBitmapWidget: public Widget
       if (rect.h >= 96 && rect.w >= 120) {
 
         if (buffer) {
-          dc->drawBitmap(0, 0, buffer.get());
+          dc->drawBitmap(0, 38, buffer.get());
         }
 
         auto iconMask = theme->getIconMask(ICON_MODEL);
@@ -99,7 +99,7 @@ class ModelBitmapWidget: public Widget
         }
 
         if (rect.h >= 96 && rect.w >= 120) {
-          buffer->drawScaledBitmap(bitmap.get(), 0, 38, width(), height() - 38);
+          buffer->drawScaledBitmap(bitmap.get(), 0, 0, width(), height() - 38);
         } else {
           buffer->drawScaledBitmap(bitmap.get(), 0, 0, width(), height());
         }
